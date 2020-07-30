b4dd2e5b5e7b3f5dad8f5f79e0e69d870c5dfb39
Yoguel Salazar <yoguel.salazar@gmail.com>
2020-07-30 15:07:19 -0500

    codeforces 660 : C

--- codeforces/Codeforces Round #660 (Div. 2)/C.cpp
+++ codeforces/Codeforces Round #660 (Div. 2)/C.cpp
@@ -3,9 +3,9 @@ using namespace std;
 #define MAX 100005
 
 int n, m;
-int p[MAX]; //personas total
+int p[MAX]; //personas
+int total[MAX];
 int h[MAX];
-int toSolve[MAX];
 bool visited[MAX];
 int bad[MAX];
 int good[MAX];
@@ -17,42 +17,32 @@ bool dfs(int i){
         return true;
     visited[i]=true;
 
-    //cout<<"v";
-    if((p[i]-h[i])%2)
-        return false;
-
-    cout<<"#"<<i<<" "<<p[i]<<" "<<h[i]<<"\n";
-
-    int realBad = (p[i]-h[i])/2;
-    int realGood = p[i]-realBad;
-
-
-    if(good[i] < realGood){
-        if(bad[i]<realGood-good[i])
-            return false;
-        bad[i] -= realGood-good[i];
-        good[i] += realGood-good[i];
-    }
-    //cout<<"#";
-
-    if(bad[i]+good[i]!=p[i])
-        return false;
-    if(bad[i]!=realBad)
-        return false;
-    //cout<<"#";
+    int sum = 0;
+    int sumGood = 0;
     for(int v : road[i]){
-        if(visited[v]) continue;
-        p[v] += p[i];
-        bad[v] += bad[i];
-        good[v] += good[i];
-        toSolve[v]--;
-        if(!toSolve[v])
-            if(!dfs(v))
-                return false;
+        if(!dfs(v))
+            return false;
+        sum+= total[v];
+        sumGood += good[v];
     }
-    //cout<<"#";
-    return true;
+    sum+=p[i];
 
+    if((h[i]+sum)%2)
+        return false;
+
+    good[i] = (h[i]+sum)/2;
+    bad[i] = sum - good[i];
+
+    //cout<<"#"<<i<<" "<<good[i]<<" "<<bad[i]<<"\n";
+
+    if(bad[i]<0)
+        return false;
+
+    if(good[i]<sumGood)
+        return false;
+
+    total[i] = sum;
+    return true;
 }
 
 void solve(){
@@ -63,9 +53,9 @@ void solve(){
         cin>>h[i];
     for(int i=1; i<=n; i++){
         road[i].clear();
-        toSolve[i]=-1;
         visited[i]=false;
         bad[i]=good[i]=0;
+        total[i] = 0;
     }
 
     for(int i=1; i<n; i++){
@@ -73,36 +63,18 @@ void solve(){
         cin>>x>>y;
         road[x].push_back(y);
         road[y].push_back(x);
-        toSolve[x]++;
-        toSolve[y]++;
     }
-    toSolve[1]++;
 
-    for(int i=1; i<=n; i++){
-        if(!toSolve[i]){
-            if((p[i] - h[i])%2){
-                cout<<"NO\n";
-                return;
-            }
+    if(dfs(1))
+        cout<<"YES\n";
+    else cout<<"NO\n";
 
-            bad[i] = (p[i] - h[i])/2;
-            good[i] = p[i] - bad[i];
-            if(good[i]<0){
-                cout<<"NO\n";
-                return;
-            }
-
-            if(!dfs(i)){
-                cout<<"NO\n";
-                return;
-            }
-        }
-    }
-    cout<<"YES\n";
     return;
 }
 
 int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
     int t; cin>>t;
     while(t--)
         solve();
