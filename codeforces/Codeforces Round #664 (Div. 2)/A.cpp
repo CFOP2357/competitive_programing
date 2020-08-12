ecc716d4fe0687ed991ff3d36c73211a806ec56c
Yoguel Salazar <yoguel.salazar@gmail.com>
2020-08-12 17:31:11 -0500

    Codeforces Round #664 (Div. 2): A

--- codeforces/Codeforces Round #664 (Div. 2)/A.cpp
+++ codeforces/Codeforces Round #664 (Div. 2)/A.cpp
@@ -13,50 +13,17 @@ ull n;
 void solve(){
     int r, g, b, w;
     cin>>r>>g>>b>>w;
-    long long total = r+g+b+w;
-    int zero = 0;
-    int impar = r%2 + g%2 + b%2 + w%2;
-    zero = !r + !g + !b + !w;
-    //cout<<zero<<"\n";
 
-    if(total%2){ //impar
-        if(zero){
-            if(zero == 3)
-                cout<<"Yes\n";
-            else if(impar==1)
-                cout<<"Yes\n";
-            else
-                cout<<"No\n";
-        }
-        else {
-            if(impar == 1 || impar==3 || impar==0)
-                cout<<"Yes\n";
-            else {
-                if(r==b && r==g)
-                    cout<<"Yes\n";
-                else cout<<"No\n";
-            }
-        }
+    int impar = r%2 + g%2 + b%2 + w%2;
+    bool zero = !(r && g && b);
+
+    if(!zero && impar !=2 ){
+        cout<<"Yes\n";
     }
-    else{ //par
-        if(zero){
-            if(zero == 3)
-                cout<<"Yes\n";
-            else if(impar>0)
-                cout<<"No\n";
-            else
-                cout<<"Yes\n";
-        }
-        else {
-            if(impar == 0 || impar==4)
-                cout<<"Yes\n";
-            else{
-                if(r==b && r==g)
-                    cout<<"Yes\n";
-                else cout<<"No\n";
-            }
-        }
+    else if(zero && impar<=1){
+        cout<<"Yes\n";
     }
+    else cout<<"No\n";
 }
 
 
