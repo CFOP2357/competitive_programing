cd517f15d12d2c3e280a1c245c95549dcc562a19
Yoguel Salazar <yoguel.salazar@gmail.com>
2020-08-14 20:43:12 -0500

    Educational Codeforces Round 93 (Rated for Div. 2): C

--- codeforces/Educational Codeforces Round 93 (Rated for Div. 2)/C.cpp
+++ codeforces/Educational Codeforces Round 93 (Rated for Div. 2)/C.cpp
@@ -7,80 +7,30 @@ using namespace std;
 typedef long long ull;
 
 inline ull subMod(ull a, ull b){
-    return ((a+MOD)-b)%MOD;
+    return (((a%MOD)+MOD)-(b%MOD))%MOD;
 }
 
-vector <ull> a;
-vector <ull> sum;
+vector <int> a;
+map<int, ull> m;
 
 void solve(){
+    m.clear();
     a.clear();
-    sum.clear();
     int n; cin>>n;
     string s; cin>>s;
-    sum.push_back(0);
-    a.push_back(0);
-    int ans = 0;
-    for(int i=0; i<n; i++){
-        a.push_back(s[i]-'0');
-        sum.push_back(sum.back()+a.back());
-        if(a.back()==1)
-            ans++;
+    for(char c : s)
+        a.push_back(c-'1');
+
+    int sum = 0;
+    ull ans = 0;
+    m[0] = 1;
+    for(int k : a){
+        sum += k;
+        ans += m[sum];
+        m[sum]++;
+        //cout<<m[sum]<<"\n";
     }
 
-    int one = 0;
-    vector<int> last;
-    for(int i=1; i<=n; i++){
-       if(a[i]==1){
-            ans += one;
-            one++;
-       }
-       else if(a[i]>0){
-         int p = a[i];
-         while(last.size() && last.back() == 0 && p>1){
-            p--;
-            last.pop_back();
-         }
-         if(p>1){
-            last.push_back(p);
-         }
-         else {
-            ans+=one;
-            one=1;
-         }
-       }
-       else {
-        last.push_back(0);
-       }
-    }
-
-    one = 0;
-    last.clear();
-    reverse(a.begin(), a.end());
-    for(int i=1; i<=n; i++){
-       if(a[i]==1){
-            ans += one;
-            one++;
-       }
-       else if(a[i]>0){
-         int p = a[i];
-         while(last.size() && last.back() == 0 && p>1){
-            p--;
-            last.pop_back();
-         }
-         if(p>1){
-            last.push_back(p);
-         }
-         else {
-            ans+=one;
-            one=1;
-         }
-       }
-       else {
-        last.push_back(0);
-        one = 0;
-       }
-    }
     cout<<ans<<"\n";
 }
 
--- codeforces/Educational Codeforces Round 93 (Rated for Div. 2)/D.cpp
+++ codeforces/Educational Codeforces Round 93 (Rated for Div. 2)/D.cpp
@@ -10,15 +10,30 @@ inline ull subMod(ull a, ull b){
     return ((a+MOD)-b)%MOD;
 }
 
-vector <ull> a;
+vector <ull> r;
+vector <ull> g;
 vector <ull> b;
 
 void solve(){
+    int R, G, B;
+    for(int i=0; i<R; i++){
+        int z; cin>>z;
+        r.push_back(z);
+    }
+    for(int i=0; i<G; i++){
+        int z; cin>>z;
+        g.push_back(z);
+    }
+    for(int i=0; i<B; i++){
+        int z; cin>>z;
+        b.push_back(z);
+    }
+
 
 }
 
 int main(){
-    int t; cin>>t;
+    int t=1;
     while(t--){
         solve();
     }
