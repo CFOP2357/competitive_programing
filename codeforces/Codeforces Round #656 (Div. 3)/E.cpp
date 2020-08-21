03c2fdcc9a7a6f3d92a61af2089e86d50dfec194
Yoguel Salazar <yoguel.salazar@gmail.com>
2020-08-20 21:29:14 -0500

    Codeforces Round #656 (Div. 3): E

--- codeforces/Codeforces Round #656 (Div. 3)/E.cpp
+++ codeforces/Codeforces Round #656 (Div. 3)/E.cpp
@@ -1,16 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define MAX 1000100
+#define MAX 200010
 #define MOD 1000000007
 
 typedef long long ull;
 
-vector<ull> a;
-vector<ull> b;
-ull n;
+vector<int> g[MAX];
+vector<pair<int, int>> edge;
+int pos[MAX];
+bool hasC;
+
+void dfs(int v, int &order){
+    if(pos[v])
+        return;
+    pos[v]=-1;
+    for(int u : g[v]){
+        if(pos[u]==-1)
+            hasC=true;
+        dfs(u, order);
+    }
+    order++;
+    pos[v] = order;
+}
 
 void solve(){
+    int n, m; cin>>n>>m;
+    for(int i=1; i<=n; i++){
+            pos[i]=0;
+            g[i].clear();
+    }
+
+    edge.clear();
+    for(int i=0; i<m; i++){
+        int t, u, v;
+        cin>>t>>u>>v;
+        if(t){
+            g[u].push_back(v);
+        }
+        edge.push_back({u, v});
+    }
+
+    hasC = false;
+    int order = 0;
+    for(int i=1; i<=n; i++){
+        if(!pos[i]){
+            dfs(i, order);
+        }
+    }
+
+    if(hasC){
+        cout<<"NO\n";
+    }
+    else{
+        cout<<"YES\n";
+        for(auto p : edge){
+            if(pos[p.first]>pos[p.second])
+                cout<<p.first<<" "<<p.second<<"\n";
+            else
+                cout<<p.second<<" "<<p.first<<"\n";
+        }
+    }
 
 }
 
