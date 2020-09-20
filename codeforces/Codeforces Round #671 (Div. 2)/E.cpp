3b062867640c3ed7a00caa213653c08cf1a8f76f
Yoguel Salazar <yoguel.salazar@gmail.com>
2020-09-20 17:22:48 -0500

    Codeforces Round #671 (Div. 2): E

--- codeforces/Codeforces Round #671 (Div. 2)/E.cpp
+++ codeforces/Codeforces Round #671 (Div. 2)/E.cpp
@@ -33,11 +33,62 @@ ull n;
 
 void solve(){
     a.clear(); b.clear();
+
     cin>>n;
-    for(int i=0; i<n; i++){
-        ull z; cin>>z;
-        a.push_back(z);
+
+
+    for(int i=2; i*i<=n; i++){
+        if(n%i == 0){
+            a.push_back(i);
+            if(i*i!=n)
+                a.push_back(n/i);
+        }
     }
+
+    sort(all(a));
+
+    vector<int> primes;
+    map<int, bool> selected;
+
+    for(int k : a){
+        if(selected[k])
+            continue;
+        primes.push_back(k);
+        selected[k] = true;
+        for(int p : a){
+            if(p%k == 0)
+                selected[p] = true;
+        }
+    }
+
+    selected.clear();
+    bool in = false;
+    for(int i = 0; i<primes.size(); i++){
+        cout<<primes[i]<<" ";
+        selected[primes[i]] = true;
+
+        for(int k : a){
+            if(selected[k] || (i+1<primes.size() && k==primes[i]*primes[i+1]) )
+                continue;
+
+            if(k%primes[i] == 0){
+                selected[k] = true;
+                cout<<k<<" ";
+            }
+        }
+
+        if(i+1<primes.size()){
+            if(primes[i]*primes[i+1] != n){
+                cout<<primes[i]*primes[i+1]<<" ";
+                selected[primes[i]*primes[i+1]] = true;
+            }
+            else in = true;
+        }
+    }
+
+    cout<<n<<"\n";
+
+    cout<<in<<"\n";
 
 }
 
