e28c380bc6277afe656cd674aabf9753e51813d6
Yoguel Salazar <yoguel.salazar@gmail.com>
2020-09-26 22:43:03 -0500

    Educational Codeforces Round 89 (Rated for Div. 2): D

--- codeforces/Educational Codeforces Round 89 (Rated for Div. 2)/D.cpp
+++ codeforces/Educational Codeforces Round 89 (Rated for Div. 2)/D.cpp
@@ -25,40 +25,49 @@ typedef long long ull;
 typedef pair<ull, ull> pii;
 typedef vector<ull> vi;
 
-#define MAX 1000100
+#define MAX 10000007
 #define MOD 1000000007
 
 vector<ull> a;
 vector<ull> b;
 ull n;
 
+int lp[MAX];
+
 void solve(){
     a.clear(); b.clear();
+
     cin>>n;
     for(int i=0; i<n; i++){
         ull z; cin>>z;
+        //cout<<z<<" ";
+        ull k = z;
+
+        int t = 0;
 
         vector<int> d;
-        for(int i=2; i*i<=z && z>1; i++){
-            if(z%i==0){
-                d.push_back(i);
-                while(z%i==0)z/=i;
-            }
+        while(z>1){
+                //cout<<z<<"\n";
+                int j = lp[z];
+                d.push_back(j);
+                //cout<<j<<"#\n";
+                while(z%j==0 && z>1){
+                        z/=j;
+                        t++;
+                }
         }
+
         if(z>1) d.push_back(z);
 
-        int p(0),r(0);
-
-        map<int, int> cong;
-        for(int u :
-
-        if(!p || !r){
+        if(d.size()==1){
             a.push_back(-1);
             b.push_back(-1);
         }
         else {
-            a.push_back(p);
-            b.push_back(r);
+            a.push_back(d[0]);
+            int p = 1;
+            for(int i=1; i<d.size(); i++) p*=d[i];
+            b.push_back(p);
         }
 
     }
@@ -69,11 +78,22 @@ void solve(){
     for(int ans : b) cout<<ans<<" ";
     cout<<"\n";
 
+
 }
 
 
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
+
+    for(long long i=2; i<MAX; i++)
+        if(!lp[i]){
+            lp[i] = i;
+            //cout<<i<<"\n";
+            for(long long j = i*i; j<MAX; j+=i){
+                lp[j] = i;
+                //cout<<j<<"\n";
+            }
+        }
 
     int t=1;
     while(t--){
