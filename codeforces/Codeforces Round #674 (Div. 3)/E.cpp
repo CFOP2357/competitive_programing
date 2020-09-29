b47f67de5532a9912f4cbee8061a4978b4b48159
Yoguel Salazar <yoguel.salazar@gmail.com>
2020-09-29 16:32:44 -0500

    Codeforces Round #674 (Div. 3): E

--- codeforces/Codeforces Round #674 (Div. 3)/E.cpp
+++ codeforces/Codeforces Round #674 (Div. 3)/E.cpp
@@ -28,36 +28,62 @@ typedef vector<ull> vi;
 #define MAX 1000100
 #define MOD 1000000007
 
-vector<ull> a;
-vector<ull> b;
 ull n;
 
-int ans1 = 0, ans2 = 0;
+int a1, a2, a3;
+int b1, b2, b3;
 
 void solve(){
 
+    int n; cin>>n;
 
-    int a1, a2, a3; cin>>a1>>a2>>a3;
-    int b1, b2, b3; cin>>b1>>b2>>b3;
+    cin>>a1>>a2>>a3;
+    cin>>b1>>b2>>b3;
 
-    int sum = a1-b2 + a2-b3 + a3-b1;
-    if(sum) ans2++;
+    {
 
-    sum = b1-a2 + b2-a3 + b3-a1;
-    if(sum<=0) ans1++;
+        vector<pair<int, int>> order;
+        order.push_back({0, 0});
+        order.push_back({0, 2});
+        order.push_back({1, 0});
+        order.push_back({1, 1});
+        order.push_back({2, 1});
+        order.push_back({2, 2});
 
+        sort(all(order));
+
+        int ans = min(a1, b2) + min(a2, b3) + min(a3, b1);
+
+        do {
+
+            int a[3] = {a1, a2, a3};
+            int b[3] = {b1, b2, b3};
+
+            for(auto p : order){
+                auto &a1 = a[p.first];
+                auto &b3 = b[p.second];
+                int d = min(a1, b3);
+                a1 -= d; b3 -= d;
+            }
+
+            ans = min(a[0] + a[1] + a[2], ans);
+
+        }while(next_permutation(all(order)));
+
+        cout<<ans<<" ";
+    }
+
+    cout<< min(a1, b2) + min(a2, b3) + min(a3, b1)<<"\n";
 }
 
 
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
 
-    int t; cin>>t;
+    int t=1;
     while(t--){
         solve();
     }
-
-    cout<<ans1<<" "<<ans2<<"\n";
 
     return 0;
 }
