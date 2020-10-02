860de5b99c90bb268eb25a7eec614df45d4614da
Yoguel Salazar <yoguel.salazar@gmail.com>
2020-10-02 15:22:23 -0500

    Codeforces Round #646 (Div. 2): E

--- codeforces/Codeforces Round #646 (Div. 2)/E.cpp
+++ codeforces/Codeforces Round #646 (Div. 2)/E.cpp
@@ -25,28 +25,74 @@ typedef long long ull;
 typedef pair<ull, ull> pii;
 typedef vector<ull> vi;
 
-#define MAX 1000100
+#define MAX 200100
 #define MOD 1000000007
 
-vector<ull> a;
-vector<ull> b;
+vector<int> adj[MAX];
+
+vector<ull> cost;
+vector<bool> init;
+vector<bool> goal;
+
 ull n;
 
-void solve(){
-    a.clear(); b.clear();
-    cin>>n;
-    for(int i=0; i<n; i++){
-        ull z; cin>>z;
-        a.push_back(z);
+ull total = 0;
+
+int dfs(int v, int p){
+    cost[v] = min(cost[v], cost[p]);
+
+    int ans = 0;
+
+    if(init[v] != goal[v]){
+        if(goal[v])
+            ans++;
+        else ans--;
     }
 
+    for(int u : adj[v]){
+        if(u==p) continue;
+
+        int res = dfs(u, v);
+
+        if( (res > 0 && ans < 0) || (res < 0 && ans > 0) )
+            total += min(abs(ans), abs(res)) * cost[v] * 2;
+
+        ans += res;
+    }
+
+    return ans;
+
+}
+
+void solve(){
+
+    cin>>n;
+    cost.push_back(LLONG_MAX); init.push_back(0); goal.push_back(0);
+    for(int i=0; i<n; i++){
+        int cost; cin>>cost;
+        bool init, goal; cin>>init>>goal;
+        ::cost.push_back(cost);
+        ::goal.push_back(goal);
+        ::init.push_back(init);
+    }
+
+    for(int i = 1; i<n; i++){
+        int u, v; cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    if(dfs(1, 0))
+        cout<<"-1\n";
+    else
+        cout<<total<<"\n";
 }
 
 
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
 
-    int t; cin>>t;
+    int t=1;
     while(t--){
         solve();
     }
