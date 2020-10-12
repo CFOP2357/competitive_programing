d8e2811b599c60a85de3c5df59b1319cc995a521
Yoguel Salazar <yoguel.salazar@gmail.com>
2020-10-11 21:58:58 -0500

    Codeforces Global Round 11: D >>>>>>>>>>>>>>>>>:VVVVVVVVVVV

--- codeforces/Codeforces Global Round 11/D.cpp
+++ codeforces/Codeforces Global Round 11/D.cpp
@@ -32,6 +32,8 @@ vector<ull> a;
 
 ull n;
 
+ull p;
+
 vector<vector<int>> ans;
 
 void rotation(vector<int> sz){
@@ -50,13 +52,28 @@ void rotation(vector<int> sz){
 
     }
 
+    int k = 0;
+    for(int i=1; i<n; i++){
+        if(b[i]-1==b[i-1])
+            k++;
+    }
+    /*if(k<=p){
+        //cout<<sz.size()<<"\n";
+        cout<<"\n";
+        for(int k : a)
+            cout<<k<<" ";
+        cout<<"\n";
+        for(int k : b)
+            cout<<k<<" ";
+        cout<<"\n";
+    } p=k;*/
+
     for(int i=0; i<n; i++)
         a[i] = b[i];
 
-    //cout<<sz.size()<<"\n";
-    for(int k : a)
-        cout<<k<<" ";
-    cout<<"\n";
+
+
+    //char c; cin>>c;
 }
 
 bool ordered(){
@@ -98,26 +115,38 @@ void solve(){
         nxt.push_back(i);
 
         int j;
-        for(j = i-1; a[j]!=a[0]-1 && j<n; j++);
 
-        if(j==n){
-           for(j = i-1; a[j]!=a[i]-1 && j<n; j++);
-           nxt.push_back(j-i-1);
-           nxt.push_back(1);
-        }
-        else if(j>=i)
+        if(a[0]==1){
+
+           int k;
+           for(k=1; k+i<n && a[i+k]-1==a[i+k-1]; k++);
+           nxt.push_back(k);
+
+           for(j = i; a[j]!=a[i]-1 && j<n; j++);
+
+           i  += k;
+           if(j>=i)
             nxt.push_back(j-i+1);
+           //nxt.push_back(1);
+           if(j<n-1)
+            nxt.push_back(n-j-1);
 
-        if(j<n-1){
-            if(a[j+1] == 1){
-                nxt.push_back(1);
-                j++;
-            }
+        }
+        else {
+            for(j = i-1; a[j]!=a[0]-1 && j<n; j++);
+
+            //if(j>=i)
+                nxt.push_back(j-i+1);
+            //nxt.push_back(1);
+
             if(j<n-1)
                 nxt.push_back(n-j-1);
+
         }
 
-        /*for(int p : nxt)
+        /*cout<<i<<" "<<j<<"\n";
+
+        for(int p : nxt)
             cout<<p<<" ";
         cout<<"\n";*/
 
@@ -140,7 +169,7 @@ void solve(){
 
 
 int main(){
-    ios_base::sync_with_stdio(0); cin.tie(0);
+    //ios_base::sync_with_stdio(0); cin.tie(0);
 
     int t=1;
     while(t--){
@@ -156,8 +185,5 @@ int main(){
 
 49
 14 48 47 19 45 32 43 42 41 40 16 44 37 36 35 34 33 38 39 30 29 28 15 26 25 24 23 22 21 20 49 18 31 17 27 46 13 12 11 10 6 8 7 9 5 4 3 2 1
-
-10
-1 2 3 4 5 7 8 9 6 10
 
 */
