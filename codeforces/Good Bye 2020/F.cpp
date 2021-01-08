34820a1b40128a1895cdfb9ce4c88e39786953b8
cfop2357 <yoguel.salazar@gmail.com>
2021-01-07 21:57:22 -0600

    Good Bye 2020: F

--- codeforces/Good Bye 2020/F.cpp
+++ codeforces/Good Bye 2020/F.cpp
@@ -31,20 +31,60 @@ typedef long long ull;
 typedef pair<ull, ull> pii;
 typedef vector<ull> vi;
 
-#define MAX 1000100
+#define MAX 500100
 #define MOD 1000000007
 
-vector<ull> a;
-vector<ull> b;
-ull n;
+struct UF {
+	vi e;
+	UF(int n) : e(n, -1) {}
+	bool sameSet(int a, int b) { return find(a) == find(b); }
+	int size(int x) { return -e[find(x)]; }
+	int find(int x) { return e[x] < 0 ? x : e[x] = find(e[x]); }
+	bool join(int a, int b) {
+		a = find(a), b = find(b);
+		if (a == b) return false;
+		if (e[a] > e[b]) swap(a, b);
+		e[a] += e[b]; e[b] = a;
+		return true;
+	}
+};
+
+vector<pii> adj;
+ull n, m;
 
 void solve(){
-    a.clear(); b.clear();
-    cin>>n;
+
+    cin>>n>>m;
     for(int i=0; i<n; i++){
-        ull z; cin>>z;
-        a.push_back(z);
+        int k, u, v; cin>>k;
+        cin>>u;
+        if(k>1) cin>>v;
+        else v = 0;
+
+        adj.push_back({u, v});
     }
+
+    UF uf(m+2);
+
+    vector<int> ans;
+    for(int i=0; i<n; i++){
+        auto s = adj[i];
+        if(!uf.sameSet(s.first, s.second)){
+            ans.push_back(i+1);
+            uf.join(s.first, s.second);
+        }
+    }
+
+    sort(all(ans));
+
+    ull p = 1; for(int i=1; i<=ans.size(); i++) {
+        p *= 2; p %= MOD;
+    }
+
+    cout<<p<<" "<<ans.size()<<"\n";
+    for(int a : ans)
+        cout<<a<<" ";
+    cout<<"\n";
 
 }
 
@@ -52,7 +92,7 @@ void solve(){
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
 
-    int t; cin>>t;
+    int t=1;
     while(t--){
         solve();
     }
