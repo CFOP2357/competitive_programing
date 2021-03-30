1d8fe19e9278c9116dee917379123cb458b411b1
cfop2357 <yoguel.salazar@gmail.com>
2021-03-29 19:35:35 -0600

    CodeCraft-21 and Codeforces Round #711 (Div. 2): D

--- codeforces/CodeCraft-21 and Codeforces Round #711 (Div. 2)/D.cpp
+++ codeforces/CodeCraft-21 and Codeforces Round #711 (Div. 2)/D.cpp
@@ -33,26 +33,57 @@ typedef long long ull;
 typedef pair<ull, ull> pii;
 typedef vector<ull> vi;
 
-#define MAX 1000100
+#define MAX (long double)100000.00
 #define MOD 1000000007
 
-vector<ull> a;
-vector<ull> b;
 ull n, m;
 string s;
 
+ull type1(ull k, ull x){
+    long double ans = (long double)k + ((long double)x)/MAX;
+    ans = ceil(ans);
+    return ans;
+}
+
+ull type2(ull k, ull x){
+    long double ans = k * x;
+    ans /= MAX;
+    ans = ceil(ans);
+    return ans;
+}
+
 void solve(){
-    a.clear(); b.clear();
+
     cin>>n>>m;
 
-    vector<vector<bool>> dp(n+1, vector<bool>(m+1, false));
+    vector<ull> ans(m+1, -1);
 
-    dp[0][0] = true;
+    ans[0] = 0;
     for(int i=1; i<=n; i++){
 
+        ull t, x, y; cin>>t>>x>>y;
 
+        for(ull j=0; j<=m; j++){
+            if(ans[j]<0 || ans[j]==i)
+                continue;
+
+            ull p;
+            if(t==1)
+                p = type1(j, x);
+            else
+                p = type2(j, x);
+
+            for(ull a = 1; a<=y && p<=m && ans[p]<0; a++, p = (t==1)?type1(p, x):type2(p, x)){
+                ans[p] = i;
+            }
+
+        }
 
     }
+
+    for(int i=1; i<=m; i++)
+        cout<<ans[i]<<" ";
+    cout<<"\n";
 
 }
 
