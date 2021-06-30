8d819f6fabf27714237af89f862e5529f53bcd71
cfop2357 <yoguel.salazar@gmail.com>
2021-06-30 18:56:13 -0500

    template

--- template.cpp
+++ template.cpp
@@ -1,3 +1,4 @@
+//#include <bits/stdc++.h>
 #include <iostream>
 #include <climits>
 #include <vector>
@@ -13,7 +14,7 @@
 #include <unordered_map>
 #include <unordered_set>
 
-#include <time.h>
+//#include <time.h>
 
 using namespace std;
 
@@ -27,25 +28,28 @@ typedef tree<ull,null_type,less_equal<ull>,rb_tree_tag, tree_order_statistics_no
 #define findBK order_of_key
 */
 
-#define all(a) a.begin(), a.end()
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
 
-typedef long long ull;
-typedef pair<ull, ull> pii;
-typedef vector<ull> vi;
 
 #define MAX 1000100
 #define MOD 1000000007
 
-vector<ull> a;
-vector<ull> b;
-ull n, m;
+vector<ll> a;
+vector<ll> b;
+ll n, m;
 string s;
 
 void solve(){
     a.clear(); b.clear();
     cin>>n;
     for(int i=0; i<n; i++){
-        ull z; cin>>z;
+        ll z; cin>>z;
         a.push_back(z);
     }
 
@@ -53,7 +57,7 @@ void solve(){
 
 
 int main(){
-    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin.tie(0)->sync_with_stdio(0); cin.exceptions(cin.failbit);
     //srand (time(NULL));
 
     int t=1; cin>>t;
