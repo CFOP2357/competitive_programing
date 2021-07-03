15aee661d2724da78f8fd19db8b54b43069c2c83
cfop2357 <yoguel.salazar@gmail.com>
2021-07-03 11:19:56 -0500

    Codeforces Round #729 (Div. 2)

---
+++ codeforces/Codeforces Round #729 (Div. 2)/B.cpp
@@ -0,0 +1,85 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <climits>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+#include <map>
+#include <set>
+
+#include <stack>
+#include <queue>
+
+#include <unordered_map>
+#include <unordered_set>
+
+//#include <time.h>
+
+using namespace std;
+
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+typedef tree<ull,null_type,less<ull>,rb_tree_tag, tree_order_statistics_node_update> orderedSet;
+typedef tree<ull,null_type,less_equal<ull>,rb_tree_tag, tree_order_statistics_node_update> orderedMultiSet;
+
+#define findBO find_by_order
+#define findBK order_of_key
+*/
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pii;
+typedef vector<ll> vi;
+
+
+#define MAX 1000100
+#define MOD 1000000007
+
+vector<ll> a;
+vector<ll> b;
+ll n, m;
+string s;
+
+void solve(){
+    ll a, b;
+    cin>>n>>a>>b;
+    //cout<<b%a<<"\n";
+    while(n>1){
+        //cout<<n<<" "<<n%a<<" "<<__gcd((b%a), (n%a))<<"\n";
+        if((n-1)%b == 0)
+            n=1;
+        else if(n%a == 0)
+            n/=a;
+        else if(n<a)
+            n = 0;
+        else if(b%a){
+            n/=a;
+        }
+        else
+            n=0;
+    }
+
+    if(n==1)
+        cout<<"Yes\n";
+    else
+        cout<<"No\n";
+}
+
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0); cin.exceptions(cin.failbit);
+    //srand (time(NULL));
+
+    int t=1; cin>>t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
+
