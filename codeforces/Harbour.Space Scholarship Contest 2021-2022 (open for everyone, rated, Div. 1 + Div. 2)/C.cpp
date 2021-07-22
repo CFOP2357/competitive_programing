96b1b1e04d59e6fc9a316ad6b211c2109d9f5307
cfop2357 <yoguel.salazar@gmail.com>
2021-07-22 12:06:58 -0500

    Harbour.Space Scholarship Contest 2021-2022 (open for everyone, rated, Div. 1 + Div. 2)

---
+++ codeforces/Harbour.Space Scholarship Contest 2021-2022 (open for everyone, rated, Div. 1 + Div. 2)/C.cpp
@@ -0,0 +1,117 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <climits>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+#include <map>
+#include <set>
+
+#include <stack>
+#include <queue>
+
+#include <unordered_map>
+#include <unordered_set>
+
+//#include <time.h>
+
+using namespace std;
+
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+typedef tree<ull,null_type,less<ull>,rb_tree_tag, tree_order_statistics_node_update> orderedSet;
+typedef tree<ull,null_type,less_equal<ull>,rb_tree_tag, tree_order_statistics_node_update> orderedMultiSet;
+
+#define findBO find_by_order
+#define findBK order_of_key
+*/
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pii;
+typedef vector<ll> vi;
+
+
+#define MAX 1000100
+#define MOD 1000000007
+
+vector<ll> a;
+vector<ll> b;
+ll n, m;
+string s;
+
+void solve(){
+    cin>>s;
+
+    int ans = 10;
+    { // a wins
+        int a=0, b=0, ra=5, rb=5;
+        int kicks = 0;
+        bool playA = true;
+        int i = 0;
+        string t = s;
+        while(ra+rb and b+rb>=a and a+ra>=b){
+            kicks++;
+            if(playA){
+                ra--;
+                if(s[i]=='1' or s[i]=='?'){
+                    a++;
+                }
+            }
+            else {
+                rb--;
+                if(s[i]=='1')
+                    b++;
+            }
+            playA = !playA;
+            i++;
+        }
+        ans = min(ans, kicks);
+    }
+    { // b wins
+        int a=0, b=0, ra=5, rb=5;
+        int kicks = 0;
+        bool playA = true;
+        int i = 0;
+        string t = s;
+        while(ra+rb and b+rb>=a and a+ra>=b){
+            kicks++;
+            if(playA){
+                ra--;
+                if(s[i]=='1'){
+                    a++;
+                }
+            }
+            else {
+                rb--;
+                if(s[i]=='1' or s[i]=='?')
+                    b++;
+            }
+            playA = !playA;
+            i++;
+        }
+        ans = min(ans, kicks);
+    }
+
+    cout<<ans<<"\n";
+
+}
+
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0); cin.exceptions(cin.failbit);
+    //srand (time(NULL));
+
+    int t=1; cin>>t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
+
---
+++ codeforces/Harbour.Space Scholarship Contest 2021-2022 (open for everyone, rated, Div. 1 + Div. 2)/D.cpp
@@ -0,0 +1,89 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <climits>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+#include <map>
+#include <set>
+
+#include <stack>
+#include <queue>
+
+#include <unordered_map>
+#include <unordered_set>
+
+//#include <time.h>
+
+using namespace std;
+
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+typedef tree<ull,null_type,less<ull>,rb_tree_tag, tree_order_statistics_node_update> orderedSet;
+typedef tree<ull,null_type,less_equal<ull>,rb_tree_tag, tree_order_statistics_node_update> orderedMultiSet;
+
+#define findBO find_by_order
+#define findBK order_of_key
+*/
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pii;
+typedef vector<ll> vi;
+
+
+#define MAX 1000100
+#define MOD 1000000007
+
+vector<ll> a;
+vector<ll> b;
+ll n, m;
+string s;
+
+void solve(){
+    string s, t;
+    cin>>s;
+    cin>>t;
+
+    s+='#';
+    t+='#';
+
+    if(t.size()>s.size()){
+        cout<<"NO\n";
+        return;
+    }
+
+    int a=0, b=0;
+    for(int i=0; i<s.size(); i++){
+        if(a<t.size() and s[i]==t[a] and i%2!=a%2)
+            a++;
+        if(b<t.size() and s[i]==t[b] and i%2==b%2)
+            b++;
+    }
+
+    if(max(a, b)==t.size())
+        cout<<"YES\n";
+    else
+        cout<<"NO\n";
+
+
+}
+
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0); cin.exceptions(cin.failbit);
+    //srand (time(NULL));
+
+    int t=1; cin>>t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
+
