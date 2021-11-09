17dabcd57607467d255a7cee1a4c9004d1a12459
cfop2357 <yoguel.salazar@gmail.com>
2021-11-08 21:17:44 -0600

    Codeforces Round #752 (Div. 2)

---
+++ codeforces/Codeforces Round #752 (Div. 2)/A.cpp
@@ -0,0 +1,78 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <climits>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+#include <map>
+#include <set>
+
+#include <stack>
+#include <queue>
+
+#include <unordered_map>
+#include <unordered_set>
+
+#include <assert.h>
+
+//#include <time.h>
+
+using namespace std;
+
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+typedef tree<ull,null_type,less<ull>,rb_tree_tag, tree_order_statistics_node_update> orderedSet;
+typedef tree<ull,null_type,less_equal<ull>,rb_tree_tag, tree_order_statistics_node_update> orderedMultiSet;
+
+#define findBO find_by_order
+#define findBK order_of_key
+*/
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef int64_t ll;
+typedef uint64_t ull;
+typedef pair<ll, ll> pii;
+typedef vector<ll> vi;
+
+
+#define MAX 1000100
+#define MOD 1000000007
+
+vector<ll> a;
+vector<ll> b;
+ll n, m;
+string s;
+
+void solve(){
+    a.clear(); b.clear();
+    cin>>n;
+
+    ll ans = 0;
+    for(int i = 1; i <= n; i++){
+        ll z; cin>>z;
+
+        if(z > i)
+            ans = max(ans, z - i);
+    }
+
+    cout<<ans<<"\n";
+
+}
+
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0); cin.exceptions(cin.failbit);
+    //srand (time(NULL));
+
+    int t=1; cin>>t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
+
---
+++ codeforces/Codeforces Round #752 (Div. 2)/D.cpp
@@ -0,0 +1,85 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <climits>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+#include <map>
+#include <set>
+
+#include <stack>
+#include <queue>
+
+#include <unordered_map>
+#include <unordered_set>
+
+#include <assert.h>
+
+//#include <time.h>
+
+using namespace std;
+
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+typedef tree<ull,null_type,less<ull>,rb_tree_tag, tree_order_statistics_node_update> orderedSet;
+typedef tree<ull,null_type,less_equal<ull>,rb_tree_tag, tree_order_statistics_node_update> orderedMultiSet;
+
+#define findBO find_by_order
+#define findBK order_of_key
+*/
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef int64_t ll;
+typedef uint64_t ull;
+typedef pair<ll, ll> pii;
+typedef vector<ll> vi;
+
+
+#define MAX 1000100
+#define MOD 1000000007
+
+vector<ll> a;
+vector<ll> b;
+ll n, m;
+string s;
+
+void solve(){
+    ll x, y;
+    cin>>x>>y;
+
+    ll n;
+
+    if(x == y)
+        n = x;
+    else if(x > y)
+        n = x + y;
+    else {
+        n = y/x;
+        n *= x;
+
+        ll d = y - n;
+        n += d/2;
+
+        assert(n%x == y%n);
+    }
+
+    cout<<n<<"\n";
+}
+
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0); cin.exceptions(cin.failbit);
+    //srand (time(NULL));
+
+    int t=1; cin>>t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
+
